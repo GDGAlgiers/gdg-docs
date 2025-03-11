d0f16db40862782d3699ada49f14385d35b34bfb
KhalilLounis <k_lounis@estin.dz>
2025-03-11 17:16:12 +0100

    refactor: update Astro configuration and integrate new documentation structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,64 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
-
-
-// https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'GDG Docs',
-        logo: {
-            src: "./src/assets/gdg-logo.svg",
+  integrations: [
+    starlight({
+      title: "GDG Docs",
+      logo: {
+        src: "./src/assets/gdg-logo.svg",
+      },
+      social: {
+        github: "https://github.com/GDGAlgiers",
+        discord: "https://discord.gg/tX2rAAvkxX",
+      },
+      sidebar: [
+        {
+          label: "Overview",
+          autogenerate: { directory: "overview" },
+          collapsed: true,
         },
-        social: {
-            github: 'https://github.com/GDGAlgiers',
-            discord: "https://discord.gg/tX2rAAvkxX",
+        {
+          label: "Code Guidelines",
+          autogenerate: { directory: "code-guidelines" },
+          collapsed: true,
         },
-        sidebar: [
-            {
-                label: 'Overview',
-                autogenerate: { directory: 'overview' },
-            },
-            {
-                label: 'Code Guidelines',
-                autogenerate: { directory: 'code-guidelines' },
-            },
-            {
-                label: 'Guides',
-                autogenerate: { directory: 'guides' },
-                // items: [
-                // 	// Each item here is one entry in the navigation menu.
-                // 	{ label: 'Example Guide', slug: 'guides/example' },
-                // ],
-            },
-            {
-                label: 'Tools',
-                autogenerate: { directory: 'tools', exclude: ['react-scan'] },
-            },
-            {
-                label: 'Workflows',
-                autogenerate: { directory: 'workflows' },
-            },
-            {
-                label: 'References',
-                autogenerate: { directory: 'references' },
-            },
-        ],
-        customCss: [
-            // Path to your Tailwind base styles:
-            './src/tailwind.css',
-        ],
-		}), tailwind({
-            // Disable the default base styles:
-            applyBaseStyles: false,
-        })],
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+          collapsed: true,
+        },
+        {
+          label: "React",
+          autogenerate: { directory: "react" },
+          collapsed: true,
+        },
+        {
+          label: "Express",
+          autogenerate: { directory: "express" },
+          collapsed: true,
+        },
+        {
+          label: "Flutter",
+          autogenerate: { directory: "flutter" },
+          collapsed: true,
+        },
+        {
+          label: "Workflows",
+          autogenerate: { directory: "workflows" },
+          collapsed: true,
+        },
+        {
+          label: "References",
+          autogenerate: { directory: "references" },
+          collapsed: true,
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
