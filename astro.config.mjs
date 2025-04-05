8c07a6c3a8abb91113cf61022da33e3cea58205d
KhalilLounis <k_lounis@estin.dz>
2025-04-05 01:03:23 +0100

    fixing some git issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -75,7 +75,7 @@ export default defineConfig({
             },
             { label: "File Uploads", link: "express/file-uploads" },
             { label: "Testing", link: "express/testing" },
-            { label: "Performance", link: "express/performance-optimization" },
+            { label: "Performance", link: "express/performance" },
             { label: "Deployment", link: "express/deployment" },
           ],
           collapsed: true,
